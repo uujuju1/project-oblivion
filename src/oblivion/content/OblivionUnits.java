53e5f36d3e2c7e5c0b6d3824825523dc5aa4b515
uujuju1 <j4224253@gmail.com>
2022-03-23 18:06:12 -0300

    one more unit to maek the bundles

--- src/oblivion/content/OblivionUnits.java
+++ src/oblivion/content/OblivionUnits.java
@@ -6,7 +6,7 @@ import mindustry.entities.bullet.*;
 import mindustry.ctype.ContentList;
 
 public class OblivionUnits implements ContentList {
-	public static UnitType slop, detra;
+	public static UnitType slop, detra, tedri;
 
 	@Override
 	public void load() {
@@ -31,7 +31,8 @@ public class OblivionUnits implements ContentList {
 			health = 450;
 			speed = 1.7f;
 			flying = true;
-			constructor = new UnitEntity::create;
+			constructor = UnitEntity::create;
+			hitSize = 8f;
 			range = 152f;
 			maxRange = range;
 			weapons.add(
@@ -43,6 +44,41 @@ public class OblivionUnits implements ContentList {
 					bullet = new ArtilleryBulletType(1.5f, 30) {{
 						lifetime = 101f;
 						collides = true;
+					}};
+				}}
+			);
+		}};
+		tedri = new UnitType("tedri") {{
+			health = 860;
+			speed = 1.5f;
+			flying = true;
+			constructor = UnitEntity::create;
+			hitSize = 14f;
+			range = 200f;
+			maxRange = range;
+			weapons.add(
+				new Weapon("oblivion-mesulfate-laser") {{
+					x = 5.75f;
+					y = -3.5f;
+					reload = 120f;
+					shootSound = Sounds.laser;
+					bullet = new LaserBulletType(50) {{
+						width = 8f;
+						length = range;
+						colors = new Color[]{Color.valueOf("AD4747"), Color.valueOf("E86F6F"), Color.valueOf("F79797")};
+					}};
+				}},
+				new Weapon("oblivion-mesulfate-cannon") {{
+					x = 0f;
+					y = 6f;
+					reload = 150f;
+					shots = 5f;
+					shotDelay = 10f;
+					shake = 3f;
+					shootSound = Sounds.shootBig;
+					bullet = new BasicBulletType(3f, 34) {{
+						width = height = 9f;
+						lifetime = 66.6f; //ohno ohfu-
 					}};
 				}}
 			);
