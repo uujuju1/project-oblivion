8f709ee5287d820651bb218ae9b548c7e37f2047
uujuju1 <j4224253@gmail.com>
2022-03-25 22:16:39 -0300

    fixing pwanets

--- src/oblivion/content/OblivionPlanets.java
+++ src/oblivion/content/OblivionPlanets.java
@@ -1,4 +1,4 @@
-package oblivion.content.*;
+package oblivion.content;
 
 import arc.graphics.*;
 import mindustry.type.*;
@@ -18,7 +18,7 @@ public class OblivionPlanets implements ContentList {
 			meshLoader = () -> new HexMesh(this, 6);
 			cloudMeshLoader = () -> new MultiMesh(
 				new HexSkyMesh(this, 69, 0.1f, 0.2f, 5, Color.white.cpy().a(0.75f), 2, 0.45f, 1.1f, 0.45f),
-				new HexSkyMesh(this, 69, 0.1f, 0.2f, 5, Color.white.cpy().a(0.50f), 2, 0.45f, 1.1f, 0.45f),
+				new HexSkyMesh(this, 69, 0.1f, 0.2f, 5, Color.white.cpy().a(0.50f), 2, 0.45f, 1.1f, 0.45f)
 			);
 			atmosphereColor = Color.valueOf("7f7f7f");
 			atmosphereRadIn = 0.02f;
