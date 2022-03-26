d926f35955c4aad192f610fe7792921d16072059
uujuju1 <j4224253@gmail.com>
2022-03-25 23:06:40 -0300

    aaaaa

--- src/oblivion/planets/LonelaPlanetGenerator.java
+++ src/oblivion/planets/LonelaPlanetGenerator.java
@@ -14,6 +14,7 @@ import mindustry.content.*;
 import mindustry.maps.planet.*;
 import mindustry.ai.BaseRegistry.*;
 import mindustry.maps.generators.*;
+import mindustry.world.environment.*;
 import mindustry.graphics.g3d.PlanetGrid.*;
 import oblivion.content.*;
 
@@ -42,6 +43,8 @@ public class LonelaPlanetGenerator extends SerpuloPlanetGenerator {
 	{OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise, OblivionBlocks.baletise},
 	};
 
+	float water = 2f / arr[0].length;
+
 	float rawHeight(Vec3 position){
 		position = Tmp.v33.set(position).scl(scl);
 		return (Mathf.pow((float)Simplex.noise3d(seed, 7, 0.5f, 1f/3f, position.x, position.y, position.z), 2.3f));
@@ -56,8 +59,6 @@ public class LonelaPlanetGenerator extends SerpuloPlanetGenerator {
 	@Override
 	public Color getColor(Vec3 position){
 		Block block = getBlock(position);
-		//replace sand with sand color
-		if(block == Blocks.sand) return Blocks.sand.mapColor;
 		return Tmp.c1.set(block.mapColor).a(1f - block.albedo);
 	}
 
@@ -71,7 +72,8 @@ public class LonelaPlanetGenerator extends SerpuloPlanetGenerator {
 		}
 	}
 
-	Block getBlock(Vec3 position){
+	@Override
+	public Block getBlock(Vec3 position){
 		float height = rawHeight(position);
 		Tmp.v31.set(position);
 		position = Tmp.v33.set(position).scl(scl);
@@ -96,57 +98,6 @@ public class LonelaPlanetGenerator extends SerpuloPlanetGenerator {
 
 	@Override
 	public void generateSector(Sector sector){sector.generateEnemyBase = false;}
-
-	@Override
-	public float getHeight(Vec3 position){
-		float height = rawHeight(position);
-		return Math.max(height, water);
-	}
-
-	@Override
-	public Color getColor(Vec3 position){
-		Block block = getBlock(position);
-		//replace salt with sand color
-		if(block == Blocks.salt) return Blocks.sand.mapColor;
-		return Tmp.c1.set(block.mapColor).a(1f - block.albedo);
-	}
-
-	@Override
-	public void genTile(Vec3 position, TileGen tile){
-		tile.floor = getBlock(position);
-		tile.block = tile.floor.asFloor().wall;
-
-		if(Ridged.noise3d(1, position.x, position.y, position.z, 2, 22) > 0.31){
-			tile.block = Blocks.air;
-		}
-	}
-
-	Block getBlock(Vec3 position){
-		float height = rawHeight(position);
-		Tmp.v31.set(position);
-		position = Tmp.v33.set(position).scl(scl);
-		float rad = scl;
-		float temp = Mathf.clamp(Math.abs(position.y * 2f) / (rad));
-		float tnoise = Simplex.noise3d(seed, 7, 0.56, 1f/3f, position.x, position.y + 999f, position.z);
-		temp = Mathf.lerp(temp, tnoise, 0.5f);
-		height *= 1.2f;
-		height = Mathf.clamp(height);
-
-		float tar = Simplex.noise3d(seed, 4, 0.55f, 1f/2f, position.x, position.y + 999f, position.z) * 0.3f + Tmp.v31.dst(0, 0, 1f) * 0.2f;
-
-		Block res = arr[Mathf.clamp((int)(temp * arr.length), 0, arr[0].length - 1)][Mathf.clamp((int)(height * arr[0].length), 0, arr[0].length - 1)];
-		if(tar > 0.5f){
-			return tars.get(res, res);
-		}else{
-			return res;
-		}
-	}
-
-	@Override
-	protected float noise(float x, float y, double octaves, double falloff, double scl, double mag){
-		Vec3 v = sector.rect.project(x, y).scl(5f);
-		return Simplex.noise3d(seed, octaves, falloff, 1f / scl, v.x, v.y, v.z) * (float)mag;
-	}
 
 	@Override
 	protected void generate(){
