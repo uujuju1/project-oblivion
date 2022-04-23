d9105d15deedbfe601cfe223e0c62709b1a0596e
uujuju1 <j4224253@gmail.com>
2022-04-23 13:43:29 -0300

    a

--- src/oblivion/blocks/production/DrawableCrafter.java
+++ src/oblivion/blocks/production/DrawableCrafter.java
@@ -6,7 +6,16 @@ import mindustry.gen.*;
 import mindustry.world.blocks.production.*;
 // a crafter that inutilizes drawers
 public class DrawableCrafter extends GenericCrafter {
-	public Cons<Building> draw = b -> {Draw.rect(region, b.x, b.y, build.rotate ? build.rotdeg() : 0f);};
+	public Cons<Building> draw = b -> {Draw.rect(region, b.x, b.y, b.rotate ? b.rotdeg() : 0f);};
+
+	public DrawableCrafter(String name, Cons<Building> draw) {
+		super(name);
+		this.draw = draw;
+	}
+	public DrawableCrafter(String name) {
+		super(name);
+		this.draw = b -> {Draw.rect(region, b.x, b.y, b.rotate ? b.rotdeg() : 0f);};
+	}
 
 	public class DrawableCrafterBuild extends GenericCrafterBuild {
 		@Override
