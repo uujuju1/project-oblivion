e345976b5f33960c49a8664bbc679d1b79c7742b
uujuju1 <j4224253@gmail.com>
2022-04-23 13:39:48 -0300

    why

--- src/oblivion/blocks/production/DrawableCrafter.java
+++ src/oblivion/blocks/production/DrawableCrafter.java
@@ -2,10 +2,11 @@ package oblivion.blocks.production;
 
 import arc.func.*;
 import arc.graphics.g2d.*;
+import mindustry.gen.*;
 import mindustry.world.blocks.production.*;
 // a crafter that inutilizes drawers
 public class DrawableCrafter extends GenericCrafter {
-	public Cons<Building> draw = b -> {Draw.rect(region, b.x, b.y, build.rotate ? build.rotdeg() : 0f)};
+	public Cons<Building> draw = b -> {Draw.rect(region, b.x, b.y, build.rotate ? build.rotdeg() : 0f);};
 
 	public class DrawableCrafterBuild extends GenericCrafterBuild {
 		@Override
