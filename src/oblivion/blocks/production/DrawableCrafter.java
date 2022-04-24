dbfc5bed99e24c546732c9661010dee596380c12
uujuju1 <j4224253@gmail.com>
2022-04-24 17:01:19 -0300

    why

--- src/oblivion/blocks/production/DrawableCrafter.java
+++ src/oblivion/blocks/production/DrawableCrafter.java
@@ -1,5 +1,6 @@
 package oblivion.blocks.production;
 
+import arc.*;
 import arc.func.*;
 import arc.graphics.g2d.*;
 import mindustry.gen.*;
@@ -7,8 +8,6 @@ import mindustry.world.blocks.production.*;
 // a crafter that inutilizes drawers
 public class DrawableCrafter extends GenericCrafter {
 	public Cons<DrawableCrafterBuild> draw;
-	public TextureRegion[] icon = new TextureRegion[]{region};
-
 	public DrawableCrafter(String name, Cons<DrawableCrafterBuild> draw) {
 		super(name);
 		this.draw = draw;
@@ -20,7 +19,7 @@ public class DrawableCrafter extends GenericCrafter {
 
 	@Override
 	public TextureRegion[] icons() {
-		return icon;
+		return new TextureRegion[]{Core.atlas.find(name + "-full")};
 	}
 
 	public class DrawableCrafterBuild extends GenericCrafterBuild {
