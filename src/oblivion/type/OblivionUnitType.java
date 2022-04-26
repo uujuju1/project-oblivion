59d2306310a92891efdcf0c447bea39cfa9910d9
uujuju1 <j4224253@gmail.com>
2022-04-26 20:43:32 -0300

    absolutely more imports

--- src/oblivion/type/OblivionUnitType.java
+++ src/oblivion/type/OblivionUnitType.java
@@ -3,11 +3,14 @@ package oblivion.type;
 import arc.func.*;
 import arc.math.*;
 import arc.util.*;
+import arc.math.geom.*;
 import arc.graphics.g2d.*;
 import mindustry.gen.*;
 import mindustry.type.*;
-import mindustry.entities.*;
+import mindustry.entities.abilities.*;
 import mindustry.graphics.*;
+
+import mindustry.Vars.*;
 
 // engineSize and engineOffset is now useless {:
 public class OblivionUnitType extends UnitType {
