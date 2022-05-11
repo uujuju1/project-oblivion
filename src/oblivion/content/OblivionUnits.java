0968973b3c5291bb1bedaa36677b002d94c2380b
Nukeguy <59600682+d689@users.noreply.github.com>
2022-05-11 18:38:44 +0700

    Update OblivionUnits.java

--- src/oblivion/content/OblivionUnits.java
+++ src/oblivion/content/OblivionUnits.java
@@ -418,7 +418,6 @@ public class OblivionUnits implements ContentList {
 			range = 304f;
 			maxRange = range;
 			hitSize = 36f;
-		        immunities.add(StatusEffects.oblivion-infested);
 			weapons.add(
 				new Weapon("oblivion-copremite-plasma-cannon") {{
 					x = 0f;
