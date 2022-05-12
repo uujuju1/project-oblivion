8c1187e7453c55208992fbe5d64f42c1a7fb0e4b
uujuju1 <j4224253@gmail.com>
2022-05-12 19:59:07 -0300

    import

--- src/oblivion/content/OblivionBlocks.java
+++ src/oblivion/content/OblivionBlocks.java
@@ -13,6 +13,7 @@ import mindustry.content.*;
 import mindustry.graphics.*;
 import mindustry.world.meta.*;
 import mindustry.world.draw.*;
+import mindustry.entities.part.*;
 import mindustry.entities.bullet.*;
 import mindustry.world.blocks.units.*;
 import mindustry.world.blocks.defense.*;
