bf2c5d6307b00f997132c059ca004e2c6f8b124a
uujuju1 <j4224253@gmail.com>
2022-05-28 12:42:12 -0300

    fixes

--- src/oblivion/entities/draw/RotorDrawer.java
+++ src/oblivion/entities/draw/RotorDrawer.java
@@ -10,11 +10,11 @@ import mindustry.type.*;
 // drawRotor eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 public class RotorDrawer {
 	public String suffix;
-	public TextureRegion region, topRegion, cellRegion;
+	public TextureRegion region, topRegion, cellRegion, blurRegion;
 	public float x = 0f, y = 0f;
 	public float speed = 1f;
-	public float deatSlowdownScl = 3f;
-	public float deatSlowdownWarmup = 0.02f;
+	public float deathSlowdownScl = 3f;
+	public float deathSlowdownWarmup = 0.02f;
 	public int bladeCount = 4;
 
 	public RotorDrawer(String suffix) {
@@ -25,6 +25,7 @@ public class RotorDrawer {
 		region = Core.atlas.find(unit.name + suffix);
 		topRegion = Core.atlas.find(unit.name + suffix + "-top");
 		cellRegion = Core.atlas.find(unit.name + suffix + "-cell", "clear");
+		blurRegion = Core.atlas.find(unit.name + suffix + "-blur");
 	}
 
 	public void draw(Unit unit) {
@@ -33,20 +34,20 @@ public class RotorDrawer {
 		float dy = unit.y + Angles.trnsy(unit.rotation - 90f, x, y);
 
 		if (unit.dead) {
-			slowdown = Mathf.approachDelta(slowdown, 1f, deatSlowdownWarmup);
-			invSlowdown = Mathf.approachDelta(slowdown, 0f, deatSlowdownWarmup);
+			slowdown = Mathf.approachDelta(slowdown, 1f, deathSlowdownWarmup);
+			invSlowdown = Mathf.approachDelta(slowdown, 0f, deathSlowdownWarmup);
 		} else{
-			slowdown = Mathf.approachDelta(slowdown, 0f, deatSlowdownWarmup);
-			invSlowdown = Mathf.approachDelta(slowdown, 1f, deatSlowdownWarmup);
+			slowdown = Mathf.approachDelta(slowdown, 0f, deathSlowdownWarmup);
+			invSlowdown = Mathf.approachDelta(slowdown, 1f, deathSlowdownWarmup);
 		}
 
-		Draw.aplha(slowdown);
+		Draw.alpha(slowdown);
 		for (int i = 0; i < bladeCount; i++) {
-			Draw.rect(region, dx, dy, (unit.rotation + unit.id + (Time.time * speed) + (360f / bladeCount * i)) / deatSlowdownScl);
-			drawCell(unit, dx, dy, (360f / bladeCount * i) / deatSlowdownScl);
+			Draw.rect(region, dx, dy, (unit.rotation + unit.id + (Time.time * speed) + (360f / bladeCount * i)) / deathSlowdownScl);
+			drawCell(unit, dx, dy, (360f / bladeCount * i) / deathSlowdownScl);
 		}
-		Draw.aplha(invSlowdown);
-		Draw.rect(region, dx, dy, unit.rotation + unit.id + (Time.time * speed) + (360f / bladeCount * i));
+		Draw.alpha(invSlowdown);
+		Draw.rect(blurRegion, dx, dy, unit.rotation + unit.id + (Time.time * speed));
 
 		Draw.reset();
 		Draw.rect(topRegion, dx, dy, unit.rotation - 90f);
