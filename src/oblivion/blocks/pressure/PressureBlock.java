280c718d8ec856937d6bcf9ba32c07e4482d5b7c
uujuju1 <j4224253@gmail.com>
2022-06-14 13:15:12 -0300

    now time to conveyor

--- src/oblivion/blocks/pressure/PressureBlock.java
+++ src/oblivion/blocks/pressure/PressureBlock.java
@@ -99,7 +99,7 @@ public class PressureBlock extends Block {
 
 		@Override
 		public void drawPressure(Color color) {
-			Draw.alpha(0.5f * getPercentage());
+			Draw.alpha(getPercentage());
 			Draw.color(color);
 			Draw.rect(pressureRegion, x, y, rotate ? rotdeg() : 0f);
 			Draw.reset();
