4f5f4995c15f67b065c970625ef5162aa5dfa983
uujuju1 <j4224253@gmail.com>
2022-06-15 23:10:43 -0300

    typo

--- src/oblivion/blocks/defense/ExplosiveWall.java
+++ src/oblivion/blocks/defense/ExplosiveWall.java
@@ -5,7 +5,7 @@ import mindustry.world.blocks.defense.*;
 
 public class ExplosiveWall extends Wall {
 	public float damage = 1f, range = 80f;
-	public float exlosiveness = 0f, flammability = 0f;
+	public float explosiveness = 0f, flammability = 0f;
 	public boolean damages = true, combusts = true;
 
 	public ExplosiveWall(String name) {
