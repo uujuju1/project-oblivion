93ee310c7497ad1c17ad85f542651de338ba64fa
uujuju1 <j4224253@gmail.com>
2022-06-20 22:58:41 -0300

    and even more fixies

--- src/oblivion/entities/bullet/DelayDamageBulletType.java
+++ src/oblivion/entities/bullet/DelayDamageBulletType.java
@@ -1,23 +1,9 @@
 package oblivion.entities.bullet;
 
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
+import arc.func.*;
 import mindustry.entities.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.entities.Effect;
-
-import static mindustry.Vars.tilesize;
-import static arc.graphics.g2d.Draw.*;
-import static arc.graphics.g2d.Lines.stroke;
-import static arc.math.Angles.*;
-import static mindustry.Vars.*;
 
 public class DelayDamageBulletType extends BulletType {
 	public Cons<Bullet> draw = b -> {};
@@ -33,7 +19,7 @@ public class DelayDamageBulletType extends BulletType {
 	@Override
 	public void update(Bullet b) {
 		if (b.time >= delayTime) {
-			Damage.damage(b.team, b.x, b.y, explodeRadius, explodeDamage);
+			Damage.damage(b.team, b.x, b.y, damageRadius, extraDamage);
 		}
 	}
 
